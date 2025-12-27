41e77e9f911f4ff01d19101d0d25fd0e5cb9ddbf
cpierquet <45103962+cpierquet@users.noreply.github.com>
2025-12-27 11:09:51 +0100

    Add files via upload

---
+++ lucide-icons/scripts/script-lucideicons.py
@@ -0,0 +1,17 @@
+#individuals pdf must be renamed 001-name.pdf (because less than 1000 files)
+#so, the merged file will be with 'correct' order
+
+import glob
+f = open("aaaa.txt","w")     #file for generated macro
+g = open("aaab.txt","w")     #file for generated macro for doc
+
+n = 1
+res = ""
+for filepath in glob.iglob('*.svg'):
+    res = r'\GenerateLucideIcons{' + str(n) + r'}{' + filepath[17:-4] + r'}' + '\n'
+    resb = r'\showcaselucideicons{' + filepath[17:-4] + r'}{' + str(n) + r'}' + '\n'
+    f.write(res)
+    g.write(resb)
+    n += 1
+f.close()
+g.close()
